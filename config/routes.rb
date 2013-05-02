dc279928dcff6d1507e66a808202a5af00634f63
Kevin Ho <keho98@gmail.com>
2013-05-01 23:17:06 -0700

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 DanceBackend::Application.routes.draw do
-
+  devise_for :users 
   resources :dances
 
 
@@ -52,7 +52,7 @@ DanceBackend::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dances#index'
 
   # See how all your routes lay out with "rake routes"
 
