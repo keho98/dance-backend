a92a0728b4a6a15548cd8e4d680f63472f996b2f
Jay Patel <jayhp9@stanford.edu>
2013-05-08 14:48:47 -0700

    wrote code for mongo connection

--- app/models/dance.rb
+++ app/models/dance.rb
@@ -1,12 +1,38 @@
+require 'mongo'
+require 'uri'
+
 class Dance < ActiveRecord::Base
   attr_accessible :foreign_id, :title, :description
   belongs_to :user
 
   def sync data
-
+  	collection = Dance.get_mongo_collection
+  	if !self.foreign_id then
+  		# if foreign_id not set yet, insert data and set the foreign_id
+  		f_id = collection.insert({"data" => data}).to_s
+  		self.foreign_id = f_id
+  	else
+  		# if foreign_id is already set, update
+  		collection.update({"_id" => self.foreign_id}, {"$data" => data})
+  	end
   end
 
   def get_data
+  	collection = Dance.get_mongo_collection
+  	collection.find("_id" => self.foreign_id)
+  end
 
+  def self.get_mongo_collection
+  	return @@collection if @@collection
+  	
+  	if ENV[MONGOHQ_URL] then	# production
+  		db = URI.parse(ENV['MONGOHQ_URL'])
+  		db_name = db.path.gsub(/^\//, '')
+  		collection_name = 'dancedata'
+  		@@collection = Mongo::Connection.new(db.host, db.port).db(db_name)[collection_name]
+  	else	# development
+  		@@collection = Mongo::Connection.new('localhost').db('formitdb')['dancedata']
+  	
+  	@@collection
   end
 end
